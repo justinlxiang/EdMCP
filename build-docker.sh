4e754e5abd98bdbbb5a5071a151a7b12b0880af5
Justin Xiang <jx372@cornell.edu>
2025-04-28 18:38:39 -0400

    merge bash scripts

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 docker build -t edmcp .
 echo "Docker image 'edmcp' built successfully" 
+docker tag edmcp justinxiang05/edmcp:latest
+docker push justinxiang05/edmcp:latest
+echo "Docker image 'edmcp' pushed to Docker Hub successfully"
--- push_docker.sh
+++
@@ -1,2 +0,0 @@
-docker tag edmcp justinxiang05/edmcp:latest
-docker push justinxiang05/edmcp:latest
